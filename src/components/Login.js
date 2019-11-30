98c39972066b6ad39be4ccb1440ed987d5925b74
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-11-30 14:07:11 -0600

    Componente Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,18 @@
-import React, {useState, useEffect } from "react";
+import React from "react";
 import { loginListener, setLoginStatus } from '../utils/events'
+import { useHistory } from 'react-router-dom'
+import useListener from "./hooks/userListener";
+import useForm from './hooks/useForm'
 const Login = (props) => {
-  const [loginData, setLogin] = useState({
-    username: '',
-    password: ''
+  const [loginData, handleChange] = useForm({
+    username: 'sa',
+    password: 'Roberto4$'
   })
-  const handleCredenciales = (e) => setLogin({
-    ...loginData,
-    [e.target.name ]: e.target.value
-  })
-
+  const history = useHistory()
   const checkLogin = (event, didLogIn) => {
-    if (didLogIn) {
+    if (didLogIn ) {
       setLoginStatus(true)
+      history.push('/registrarUsuario')
     } else {
       setLoginStatus(false)
     }
@@ -25,17 +25,13 @@ const Login = (props) => {
     
   }
 
-  useEffect(() => {
-    listeners.listener()
-
-    return () => listeners.clear()
-  })
+  useListener(listeners)
   return (
     <div className="ui container">
       <h1>Login</h1>
       <form onSubmit={submitLogin}>
-        <input type="text" name='username' value={loginData.username} onChange={handleCredenciales} ></input>
-        <input type="password" name="password" value={loginData.password} onChange={handleCredenciales} ></input>
+        <input type="text" name='username' value={loginData.username} onChange={handleChange} ></input>
+        <input type="password" name="password" value={loginData.password} onChange={handleChange} ></input>
         <input type="submit" value="Submit" />
       </form>
        
