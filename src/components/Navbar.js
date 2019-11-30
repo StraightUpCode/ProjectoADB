9b84d380cca340179e47b5ba56154076d4c9a920
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-11-30 14:07:20 -0600

    TODO :
    Generar NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom"
-const Navbar = () => {
+
+const createNavbar = (permisos) => {
+  const rutas = []
+  for (const permiso of permisos) {
+    
+  }
+  
+}
+
+const Navbar = ( rutas ) => {
   return (
     <div className="ui secondary menu">
       <div className="header item">Menu</div>
-      <a to='/'>Home</a>
+      {/*
+           <a to='/'>Home</a>
       <a to='/facturas'>Facturas</a>
       <a to='/inventario'>Inventario</a>
       <a to='/inventario'>Configuracion</a>
+      */}
+      { rutas }
     </div>
   );
 };
