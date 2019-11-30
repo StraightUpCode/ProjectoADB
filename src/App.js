55aebba75fa074544147bbb66728c3534c7cbc49
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-11-30 14:06:44 -0600

    Nuevas Ruta crear usuario

--- src/App.js
+++ src/App.js
@@ -2,44 +2,46 @@ import React, { Component } from "react";
 import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom'
 import { status } from './utils/events'
 import Login from './components/Login'
-import Navbar from "./components/Navbar";
 import Setup from './components/Setup'
+import RegistrarUsuario from './components/RegisterUser'
 import "./custom.css";
 
-const RequireValidDB = ({children, ...rest}) => { 
+const RequireValidDB = ({ children, ...rest }) => {
   return (
     <Route {...rest}>
-      {! status.validDb ? <Redirect to='/setup' /> : children}
-   </Route>
+      {!status.validDb ? <Redirect to='/setup' /> : children}
+    </Route>
   )
 }
 
-const RequireLogin = ({ children, ...rest }) => {
-
+const RequireLogin = (props) => {
+  const {children , ...rest} = props
   return (
-    <RequireValidDB {...rest} >
-      { ! status.login ? <Redirect to='/login'/> : children} 
-    </RequireValidDB>
+    <Route {...rest}>
+      {!status.login ? <Redirect to='/login' /> : children}
+    </Route>
   )
+  
 }
 class App extends Component {
   render() {
+    console.log(RegistrarUsuario)
     return (
-      <div>
+      <>
         <Router>
-          <Navbar />
           <Switch>
             <Route path='/setup'>
               <Setup/>
             </Route>
             <RequireValidDB path='/login'>
               <Login></Login>
-            </RequireValidDB>
-            <RequireLogin path='/' >
+            </RequireValidDB>    
+            <RequireLogin path='/registrarUsuario' >
+              <RegistrarUsuario></RegistrarUsuario>
             </RequireLogin>
           </Switch>
         </Router>
-      </div>
+      </>
     );
   }
 }
