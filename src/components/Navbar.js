19df6aa851b5780347b4a8ee041a27860f4a6763
StraightUpCode <sanchez.roberto0810@gmail.com>
2019-12-05 23:11:12 -0600

    WIP
    Crear Navbar en base a la info recibida de login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
-const createNavbar = (permisos) => {
+
+let Navbar
+export const createNavbar = (permisos) => {
   const rutas = []
+  //arreglo de permisos
   for (const permiso of permisos) {
-    
+     //permiso es un objeto que contiene, el nombre de la tabla 
+    // y el valor crud
+    const miniRutas = []
+    const linkBase = ''
+    const crud = permiso.crud.toString(2)
+    for (let index = 0; index < crud.length; index += 1) {
+      if (crud.charAt(index) == '1') {
+        switch (index) {
+          case 0: {
+            miniRutas.push('borrar')
+            break;
+          }
+          case 1: {
+            miniRutas.push('actualizar')
+            break;
+          }
+          case 2: {
+            miniRutas.push('anadir')
+            break;
+          }
+          case 3: {
+            miniRutas.push('ver')
+            break;
+          }
+        }
+      }
+    }
+    for (const accion of miniRutas) {
+      const link = `/${permiso.tabla}/${accion}`
+      rutas.push((<a href={link} ></a>))
+    }
   }
+
+  Navbar = (
+    <div>
+      {rutas}
+    </div>
+  )
   
 }
 
-const Navbar = ( rutas ) => {
-  return (
-    <div className="ui secondary menu">
-      <div className="header item">Menu</div>
-      {/*
-           <a to='/'>Home</a>
-      <a to='/facturas'>Facturas</a>
-      <a to='/inventario'>Inventario</a>
-      <a to='/inventario'>Configuracion</a>
-      */}
-      { rutas }
-    </div>
-  );
-};
+export default Navbar; 
 
-export default Navbar;
